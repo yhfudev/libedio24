e3321d95969f161cba8e1a0e41104c4d3ceecda9
yhfudev <yhfudev@gmail.com>
2018-03-23 13:19:29 -0400

    update build scripts

--- autogen.sh
+++ autogen.sh
@@ -31,36 +31,29 @@ autoconf
 autoreconf -f -i -Wall,no-obsolete
 autoreconf -f -i -Wall,no-obsolete
 
-#./configure
-
-#./configure --enable-debug
 if [ 1 = 1 ]; then
-  #./configure --disable-shared --enable-static --enable-coverage --enable-valgrind
-  ./configure --disable-shared --enable-static
+  #./configure --enable-debug --enable-coverage --enable-valgrind --disable-shared --enable-static
+  ./configure --disable-debug --disable-coverage --disable-valgrind --disable-shared --enable-static
 
+  make clean
+  make ChangeLog
+  make dist-gzip
+  #make -C doc/latex/
 else
   if [ ! -d cpp-ci-unit-test ]; then
     git clone -q --depth=1 https://github.com/yhfudev/cpp-ci-unit-test.git
   fi
+  cd cpp-ci-unit-test && git pull && cd ..
   if [ ! -d libuv ]; then
     git clone -q --depth=1 https://github.com/libuv/libuv.git
   fi
-  cd cpp-ci-unit-test && git pull && cd ..
   cd libuv && git pull && ./autogen.sh && ./configure && make clean && make -j8 && cd ..
 
   which "$CC" || CC=gcc
   which "$CXX" || if [[ "$CC" =~ .*clang.* ]]; then CXX=clang++; else CXX=g++; fi
-  CC=$CC CXX=$CXX ./configure --disable-shared --enable-static --enable-valgrind --enable-coverage --with-libuv-include=`pwd`/libuv/include --with-libuv-lib=`pwd`/libuv/ --with-ciut=`pwd`/cpp-ci-unit-test
-  make clean; make; make coverage CC=$CC CXX=$CXX; make check CC=$CC CXX=$CXX; make check-valgrind CC=$CC CXX=$CXX
+  CC=$CC CXX=$CXX ./configure --disable-shared --enable-static --disable-debug --enable-coverage --enable-valgrind --with-ciut=`pwd`/cpp-ci-unit-test --with-libuv-include=`pwd`/libuv/include --with-libuv-lib=`pwd`/libuv/
+  make clean; make -j 8 coverage CC=$CC CXX=$CXX; make check CC=$CC CXX=$CXX; make check-valgrind CC=$CC CXX=$CXX
 fi
 
-#make clean
-#make ChangeLog
-
-#make
-
-#make check
-#make -C doc/latex/
-#make dist-gzip
 
 fi
